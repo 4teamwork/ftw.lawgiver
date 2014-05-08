82538008a6af5573cf9d260e175f058b3f3c33e8
David Erni <david.erni@4teamwork.ch>
2014-05-08 12:07:35 +0200

    Display role-overlay for ftw.participation participants tab.

--- ftw/lawgiver/browser/resources/sharing.js
+++ ftw/lawgiver/browser/resources/sharing.js
@@ -1,15 +1,28 @@
 jQuery(function($){
 
+  var strip_url_params = function () {
+    var index = 0,
+        url = location.href;
+    index = url.indexOf('?');
+    if(index == -1){
+        index = url.indexOf('#');
+    }
+    if(index != -1){
+        url = url.substring(0, index);
+    }
+    return url;
+  };
+
   var setup_overlay = function() {
-    var $th = $(this);
-    var role = $.trim($th.text());
-    var url = location.href.concat(
+    var $elem = $(this);
+    var role = $.trim($elem.text());
+    var url = strip_url_params().concat(
         '/lawgiver-sharing-describe-role?role='.concat(
             encodeURIComponent(role)));
     var $link = $('<a>', {
       text: role,
       href: url});
-    $th.html($link);
+    $elem.html($link);
 
     $link.prepOverlay({
       subtype: 'ajax',
@@ -19,5 +32,10 @@ jQuery(function($){
 
   $('body.template-sharing').find('table#current-user-group-sharing th').slice(1).each(setup_overlay);
   $('body.template-sharing').find('table#user-group-sharing th').slice(1).each(setup_overlay);
+  if ($('.tabbedview-tabs #tab-participants')) {
+    $(document).ajaxComplete(function() {
+      $('#participants_overview td.roleNames span.roleName').each(setup_overlay);
+    });
+  }
 
 });
