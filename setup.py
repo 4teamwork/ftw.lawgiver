68a32c59b10a477895fcc6f0479e61b6b41419cb
Jonas Baumann <jone@jone.ch>
2014-05-19 14:21:19 +0200

    Add missing Plone 4.2 classifier to package info.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(name='ftw.lawgiver',
 
       classifiers=[
         'Framework :: Plone',
+        'Framework :: Plone :: 4.2',
         'Framework :: Plone :: 4.3',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
