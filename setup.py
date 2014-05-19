131de70509ebf3adaa58b3645085507953e83a9b
Jonas Baumann <jone@jone.ch>
2014-05-19 14:08:35 +0200

    Declare missing test dependency to plone.testing.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ tests_require = [
     'plone.app.testing',
     'plone.browserlayer',
     'plone.mocktestcase',
+    'plone.testing',
     'transaction',
     'unittest2',
     'zope.configuration',
