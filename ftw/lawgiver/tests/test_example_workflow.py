f7761bb5106c6e3b1d9c2580c93bc6afc25277e7
Jonas Baumann <jone@jone.ch>
2014-12-05 15:01:06 +0100

    Split up example workflow into a Plone 4.2 version and a 4.3 version.
    The "Edit comments" version was added in 4.3.x.
    In order to get the tests to pass in both version, we need two distinct definition.xml's.

--- ftw/lawgiver/tests/test_example_workflow.py
+++ ftw/lawgiver/tests/test_example_workflow.py
@@ -1,8 +1,16 @@
 from ftw.lawgiver.testing import LAWGIVER_INTEGRATION_TESTING
 from ftw.lawgiver.tests.base import WorkflowTest
+from Products.CMFPlone.utils import getFSVersionTuple
 
 
 class TestExampleWorkflow(WorkflowTest):
 
-    workflow_path = 'assets/example'
     layer = LAWGIVER_INTEGRATION_TESTING
+
+    @property
+    def workflow_path(self):
+        plone_version = getFSVersionTuple()
+        if plone_version > (4, 3):
+            return 'assets/example-4.3'
+        else:
+            return 'assets/example-4.2'
