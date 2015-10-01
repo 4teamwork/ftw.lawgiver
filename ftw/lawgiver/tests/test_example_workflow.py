b56ddebb09b5ded9446926d7b53aabed848581c2
Jonas Baumann <jone@jone.ch>
2015-10-01 15:47:50 +0200

    Drop Plone 4.3.4 specific tests.
    The Plone 4.3.4 specific tests were added because plone.app.iterate was
    added as a dependency in Plone 4.3.5, which introduced new permissions.
    
    Since we now have the `Plone` egg as dependency, we always have those
    dependencies installed and therefore have those permissions.
    This makes the definition.xml of the example-workflow under test be the
    same again for Plone 4.3.4 and Plone 4.3.5, making the Plone 4.3.4 test
    obsolete.

--- ftw/lawgiver/tests/test_example_workflow.py
+++ ftw/lawgiver/tests/test_example_workflow.py
@@ -10,9 +10,7 @@ class TestExampleWorkflow(WorkflowTest):
     @property
     def workflow_path(self):
         plone_version = getFSVersionTuple()
-        if plone_version >= (4, 3, 5):
-            return 'assets/example-4.3.5'
-        elif plone_version > (4, 3):
-            return 'assets/example-4.3.4'
+        if plone_version >= (4, 3):
+            return 'assets/example-4.3'
         else:
             return 'assets/example-4.2'
